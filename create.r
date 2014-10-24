a0d7536440b18c70a788f62620cf9f05b20c1aa1
kasperwelbers <kasperwelbers@gmail.com>
2014-10-24 19:23:14 +0200

    a dirty start

--- create.r
+++ create.r
@@ -1,16 +1,43 @@
 library(knitr)
-?knitr
+library(tm)
+library(topicmodels)
+library(Matrix)
 
-knit("test.Rmd")
+source('topicmodel_lib.r')
+
+load('beer.rdata') # engelse liedjes in de nederlandse top 40 met het woord "beer".
+dtm = dtm.create(tokens$aid, terms=tokens$lemma)
+
+tstats = term.statistics(dtm)
+tstats = tstats[tstats$termfreq > 5 & tstats$reldocfreq < 0.25 & tstats$nonalpha==F,]
+
+dtm = dtm[,colnames(dtm) %in% tstats$term]
+m = LDA(dtm, k=3, method='Gibbs')
+
+docsums = documentsums(m)
+meta = meta[match(m@documents, meta$id),]
+
+### topic overview html
 e = new.env()
-e$topic = "bla"
+e$topic_ids = 1:3
+e$m = m
+e$date = meta$date
+e$docsums = docsums
+
+knit2html("topic_overview_template.Rmd", envir=e, )
+
+
+### articles html
+wa = getWordAssignments(m=m)
+tokens2 = merge(tokens, wa, by.x=c('aid','lemma'), by.y=c('article_id','term'), all.x=T)
+head(tokens2)
+
+e = new.env()
+e$tokens = data.frame(token=tokens2$lemma, token_index=tokens2$id, article_id=tokens2$aid, topic=tokens2$topic)
+e$meta = meta
+e$unique_ordered_aids = unique(tokens$aid)
 css = '.hip {color:red}'
 
-knit2html("test.Rmd", envir=e, stylesheet=css)
-knit2html()
-e$wc <- function(words, freqs=rep(1, length(words))) {
-  library(RColorBrewer)
-  library(wordcloud)
-  pal <- brewer.pal(6,"YlGnBu")
-  wordcloud(words, freqs, scale=c(2,.1), min.freq=1, max.words=Inf, random.order=FALSE, rot.per=.15, colors=pal)
-}
+knit2html("articles_template.Rmd", envir=e, stylesheet=css)
+
+#Je topic was: `r topic`!
---
+++ token_lib.r
@@ -0,0 +1,29 @@
+
+tagTokens <- function(token, topic){
+  token = addHtmlTags(token, topic)
+  addEmptySpaces(token)
+}
+
+buildArticle <- function(token, token_index, topic, article_id=NULL, headline=NULL, date=NULL, maxwords=NULL){
+  header = paste(headline, '<h3><i> (',article_id, ')</i></h3>', '<i> ', date, '</i>', sep='')
+  if(!is.null(maxwords)) art = na.omit(token[order(token_index)][1:maxwords]) else art = token[order(token_index)]
+  art = paste(art, collapse='')
+  if(!is.null(maxwords)) art = paste(art, '[...]')
+  paste(header, art)
+}
+
+addHtmlTags <- function(token, topic){
+  token = as.character(token)
+  notna = which(!is.na(topic))
+  token[notna] = paste("<a href='~/", topic[notna], "'>",
+                        "<span title='", topic[notna], "'>", token[notna], 
+                        "</span></a>", sep="")
+  token
+}
+
+addEmptySpaces <- function(tagged_word){
+  nospace = ifelse(substr(tagged_word, 1, 1) %in% c(',','.','"', "'", ':', ';', '?', '!', ')', '-', '`'), 1, 0)
+  tagged_word[!nospace] = paste(' ', tagged_word[!nospace], sep='') 
+  tagged_word
+}
+
