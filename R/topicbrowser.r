41fa78c5c9f975ea162e7a6ce5112a59331f8806
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-10-27 23:12:10 +0100

    Refactored into package structure

--- R/topicbrowser.r
+++ R/topicbrowser.r
@@ -1,23 +1,17 @@
-library(RColorBrewer)
-library(wordcloud)
-library(Matrix)
-library(reshape2)
-
-source("plots.r")
-
 #' Create a topic browser
 #' 
 #' This will create a set of linked html pages to browse the given topic model
 #' 
 #' @param m: the fitted LDA model object from the topicmodels package
-#' @param terms: a vector of terms, which should be sorted in their original order and match m@terms
-#' @param documents: a vector of the same length as terms, indicating the document of each term, matching m@documents
+#' @param terms: a vector of terms, which should be sorted in their original order and match m@@terms
+#' @param documents: a vector of the same length as terms, indicating the document of each term, matching m@@documents
 #' @param meta: a data frame with meta data about the documents, should have columns aid, headline, medium, and date (todo: make more flexible)
 #' @param folder_name: the name of the folder to save the browser to
 #' @param topic_ids: optionally restrict output to a selection of topics
 #' @param date_interval: specify the interval for plotting the meta$data
 #' @param words: if given, use instead of terms for displaying document
-#' @return: (invisible) the file url of the index page
+#' @return (invisible) the file url of the index page
+#' @export
 createTopicBrowser <- function(m, terms, documents, meta, folder_name=NULL, topic_ids=1:m@k, date_interval='year', words=terms) {
   wordassignments = data.frame(aid = m@documents[m@wordassignments$i], 
                                term = m@terms[m@wordassignments$j], 
@@ -51,29 +45,31 @@ createTopicBrowser <- function(m, terms, documents, meta, folder_name=NULL, topi
   invisible(file.path("file:/", folder_name, "index.html"))
 }
 
-topic_filename <- function(topic) paste("t", topic, ".html", sep="")
 
-getWordAssignments <- function(m, article_ids=m@documents){
-  docfilter = m@wordassignments$i %in% which(m@documents %in% article_ids)
-  data.frame(aid = m@documents[m@wordassignments$i[docfilter]], 
-             term = m@terms[m@wordassignments$j[docfilter]], 
-             topic = m@wordassignments$v[docfilter])
-}
+### HTML rendering
 
-##################################################
-########## TOPIC OVERVIEW FUNCTIONS ##############
-##################################################
+#' Render the index page
+#' @param topics_per_term: a matrix of terms by topics
+#' @param topics_per_doc: a matrix of documents by topics
+#' @param meta: a data frame with meta information about the documents
+#' @param topic_ids: a vector of topic ids to render
+#' @param date_interval: optional date interval to use for time graphs
+#' @return a raw html string (character)
 render_overview <- function(topics_per_term, topics_per_doc, meta, topic_ids, date_interval='year') {
-  TEMPLATE="topic_overview_template.Rmd"
+  TEMPLATE = system.file("template/index.Rmd", package="topicbrowser", mustWork=T)
   css = get_css(topic_ids)
   knit2html(text=readLines(TEMPLATE, warn=F), stylesheet=css, quiet = T)
 }
 
-##################################################
-######### ARTICLES FUNCTIONS ############
-##################################################
-
 #' Render a single topic
+#' @param topic_id: the topic id to render
+#' @param tokens_topics: a data frame with tokens (id, aid, term, word)
+#' @param meta: a data frame with meta information about the documents
+#' @param topics_per_term: a matrix of terms by topics
+#' @param topics_per_doc: a matrix of documents by topics
+#' @param nmaxdoc: the number of top documents to show
+#' @param date_interval: optional date interval to use for time graphs
+#' @return a raw html string (character)
 render_topic <- function(topic_id, tokens_topics, meta, topics_per_doc, topics_per_term, nmaxdoc=10, date_interval='year') {
   topicass = topics_per_doc[topic_id,]
   docs = names(head(topicass[order(-topicass)], n=nmaxdoc))
@@ -86,7 +82,7 @@ render_topic <- function(topic_id, tokens_topics, meta, topics_per_doc, topics_p
   top_articles = lapply(docs, render)
   css = get_css(rownames(topics_per_doc))
 
-  TEMPLATE="articles_template.Rmd"
+  TEMPLATE = system.file("template/topic.Rmd", package="topicbrowser", mustWork=T)
   knit2html(text=readLines(TEMPLATE, warn=F), stylesheet=css, quiet = T)
 }
 
@@ -98,23 +94,28 @@ render_topic <- function(topic_id, tokens_topics, meta, topics_per_doc, topics_p
 #' @param fragment.only: passed to knit2html (if T, do not output html header etc)
 #' @return a raw html character vector
 render_article <- function(terms, topics, meta, fragment.only=T) {
-  TEMPLATE = "article.Rmd"
+  TEMPLATE = system.file("template/article.Rmd", package="topicbrowser", mustWork=T)
   tokens = tagTokens(terms, topics)
   out = knit2html(text=readLines(TEMPLATE, warn=F), fragment.only=fragment.only, quiet = T)
   out
 }
 
+### HTML functions
+
+#' Get the filename for a topic
+topic_filename <- function(topic) paste("t", topic, ".html", sep="")
+
 #' Add title, href, and class to tokens
 #' 
 #' @param tokens: a vector of words
 #' @param topics: a vector of topics, can include NA's for words without a topic
-#' @return: a vector of raw html per token
+#' @return a vector of raw html per token
 tagTokens <- function(tokens, topics){
   tokens = as.character(tokens)
   tokens = gsub("`", "'", tokens)
 
   # add hrefs
-  tokens = ifelse(is.na(topics), tokens, paste("<a href='t", topics, ".html'>", tokens, "</a>", sep=""))
+  tokens = ifelse(is.na(topics), tokens, paste("<a href='", topic_filename(topics), "'>", tokens, "</a>", sep=""))
   # add span class and title
   tokens = paste("<span",
                  ifelse(is.na(topics), 
@@ -127,7 +128,7 @@ tagTokens <- function(tokens, topics){
 
 
 get_css <- function(topic_ids) {
-  CSS_TEMPLATE="style.css"
+  CSS_TEMPLATE = system.file("template/style.css", package="topicbrowser", mustWork=T)
   css = readLines(CSS_TEMPLATE, warn=F)
   colo = substr(rainbow(length(topic_ids)), 1,7)
   colorcss = paste(".t", topic_ids, " {background-color: ",colo, "}", sep="")
