e6ec1ebe82d47e48e09094bb030d4254d4b68a3e
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-04-14 23:54:39 +0200

    refactored pf into plot_, date_ into time_, info into cluster_info, and some minor changes

--- R/plots.r
+++ R/plots.r
@@ -9,47 +9,19 @@
 #' @param date_label a string indicating what the date object is in the meta data.
 #' @return nothing, only plots
 #' @export
-pf.wordcloud_time <- function(info, topic_nr, date_interval=NULL, date_label='date') {
+plot_wordcloud_time <- function(clusterinfo, topic_nr, ...) {
   par(mar = c(4.5, 3, 2, 1), cex.axis = 1.7)
   layout(matrix(c(1, 1, 2, 2), 2, 2, byrow = TRUE), widths = c(2.5, 1.5), heights = c(1, 2))  
-  pf.time(info, topic_nr, date_interval, date_label)
+  plot_time(clusterinfo=clusterinfo, topic_nr=topic_nr, ...)
   par(mar=c(0,0,0,0))
-  pf.wordcloud(info, topic_nr)
+  plot_wordcloud(clusterinfo=clusterinfo, topic_nr=topic_nr)
   par(mfrow = c(1, 1), mar=c(5,4,4,2) + 0.1) # reset to default
 }
 
-#' Standardized plotting function that can be passed to the plotfunction arguments in the \code{\link{createTopicBrowser}} function.
-#' 
-#' One of the standardized plotting function used in the Topicbrowser package to manage how topics and additional information are visualized.
-#' This specific function plots a wordcloud
-#' 
-#' @param info The output of the \code{\link{clusterinfo}} function
-#' @param topic_nr the index number of a topic
-#' @return nothing, only plots
-#' @export
-pf.wordcloud <- function(info, topic_nr) {
-  plot.wordcloud(info$topics_per_term, topic_nr)
-}
 
-#' Standardized plotting function that can be passed to the plotfunction arguments in the \code{\link{createTopicBrowser}} function.
-#' 
-#' One of the standardized plotting function used in the Topicbrowser package to manage how topics and additional information are visualized.
-#' This specific function plots a graph showing the number of words assigned to the topic per time period
-#' 
-#' @param info The output of the \code{\link{clusterinfo}} function
-#' @param topic_nr the index number of a topic
-#' @param date_interval if not NULL, a string indicating what interval to use for plotting the time/date graph. Can be 'year', 'month', 'week' and 'day'. If NULL, the date_interval will be chosen based on the number of days in the analysis.
-#' @param date_label a string indicating what the date object is in the meta data.
-#' @return nothing, only plots
-#' @export
-pf.time <- function(info, topic_nr, date_interval=NULL, date_label='date') {
-  date_var = info$meta[,date_label]
-  if(is.null(date_interval) & class(date_vector) == 'Date') date_interval = selectDateInterval(date_var)
-  plot.time(info$topics_per_doc, topic_nr, date_var, date_interval=date_interval) 
-}
 
-pf.topicdistribution <- function(info, topic_nr){
-  topic_pct_per_document = info$topics_per_doc[topic_nr,] / colSums(info$topics_per_doc)
+plot_topicdistribution <- function(clusterinfo, topic_nr){
+  topic_pct_per_document = clusterinfo$topics_per_doc[topic_nr,] / colSums(clusterinfo$topics_per_doc)
   par(mar=c(5,6,1,4)) # Set the most suitable margins
   hist(topic_pct_per_document, main='', xlab='% of document assignments', ylab='Number of documents')
   par(mar=c(5,4,4,2) + 0.1) # reset to default
@@ -65,9 +37,9 @@ pf.topicdistribution <- function(info, topic_nr){
 #' @param topic_nr the index number of a topic
 #' @return nothing, only plots
 #' @export
-pf.semnet <- function(info, topic_nr, backbone_alpha=0.05, nwords=100) {
+plot_semnet <- function(clusterinfo, topic_nr, backbone_alpha=0.05, nwords=100) {
   require(semnet)
-  dtm = createTopicDtm(info$topics_per_term, info$wordassignments, topic_nr, nwords)
+  dtm = createTopicDtm(clusterinfo$topics_per_term, clusterinfo$wordassignments, topic_nr, nwords)
   adj = wordAdjacency(dtm, measure='cosine')
   g = graph.data.frame(adj, directed=F)
   g = getBackboneNetwork(g, alpha=backbone_alpha)
@@ -99,14 +71,12 @@ createTopicDtm <- function(topics_per_term, wordassignments, topic_nr, nwords){
 
 
 ### Basic plotting functions
-selectDateInterval <- function(date_var){ 
-  ndays = as.numeric(difftime(min(date_var), max(date_var), units='days'))
-  date_interval = 'year'
-  if(ndays < 20*30) date_inteval = 'month'
-  if(ndays < 20*7) date_inteval = 'week'
-  if(ndays < 20) date_inteval = 'day'
-  #message('Date interval = ', date_interval)
-  date_interval
+selectDateInterval <- function(time_var){ 
+  ndays = as.numeric(difftime(min(time_var), max(time_var), units='days'))
+  if(ndays < 20*30) return("month")
+  if(ndays < 20*7) return('week')
+  if(ndays < 20) return('day')
+  "year"
 }
 
 prepare.time.var <- function(time_var, date_interval){
@@ -127,9 +97,9 @@ prepare.time.var <- function(time_var, date_interval){
 #' @param date_interval The date_interval is required to know what the gaps are
 #' @return A data.frame with the columns 'time' (Date) and 'value' (numeric)  
 #' @export
-fill.time.gaps <- function(d, date_interval){
+fill.time.gaps <- function(d, time_interval){
   if(class(d$time) == 'numeric') time_sequence = min(d$time):max(d$time)
-  if(class(d$time) == 'Date') time_sequence = seq.Date(from=min(d$time), to=max(d$time), by=date_interval)
+  if(class(d$time) == 'Date') time_sequence = seq.Date(from=min(d$time), to=max(d$time), by=time_interval)
   
   emptytime = time_sequence[!time_sequence %in% d$time]
   
@@ -149,18 +119,74 @@ prepare.plot.values <- function(document_topic_matrix, break_var, topic_nr, pct=
   d
 }
 
-plot.time <- function(document_topic_matrix, topic_nr, time_var, date_interval='day', pct=F, value='total', return.values=F){
+
+
+#' Standardized plotting function that can be passed to the plotfunction arguments in the \code{\link{createTopicBrowser}} function.
+#' 
+#' One of the standardized plotting function used in the Topicbrowser package to manage how topics and additional information are visualized.
+#' This specific function plots a graph showing the number of words assigned to the topic per time period
+#' 
+#' @param info The output of the \code{\link{clusterinfo}} function
+#' @param topic_nr the index number of a topic
+#' @param time_interval a string indicating what interval to use for plotting the time/date graph. Can be 'year', 'month', 'week' and 'day'. If 'auto', the date_interval will be chosen based on the number of days in the analysis.
+#' @param time_var either a vector containing the date for each document, or the column name of the date variable in the metadata stored in the clusterinfo object
+#' @return nothing, only plots
+#' @export
+plot_time <- function(lda_model=NULL, document_topic_matrix=NULL, clusterinfo=NULL, topic_nr, time_var='date', time_interval='auto', pct=F, value='total') {
+
+  if (is.null(document_topic_matrix)) {
+    if (is.null(clusterinfo)) {
+      if (is.null(lda_model)) stop("Either lda_model, document_topic_matrix, or clusterinfo needs to be specified")
+      clusterinfo = clusterinfo(lda_model)
+    }
+    document_topic_matrix = clusterinfo$topics_per_doc
+  }
+  
+  if (is.character(time_var) & length(time_var) == 1) {
+    if (is.null(clusterinfo)) stop("Time var is a column name, but clusterinfo not specified")
+    if (is.null(clusterinfo$meta)) stop("Time var is a column name, but clusterinfo metadata is not specified")
+    
+    time_var = clusterinfo$meta[,time_var]
+  }
+  if (time_interval == 'auto') time_interval = selectDateInterval(time_var)
+  
   par(mar=c(3,3,3,1))
-  time_var = prepare.time.var(time_var, date_interval)  
+  time_var = prepare.time.var(time_var, time_interval)  
   d = prepare.plot.values(document_topic_matrix, break_var=time_var, topic_nr=topic_nr, pct=pct, value=value)
   colnames(d) = c('time','value')
-  d = fill.time.gaps(d, date_interval)
+  d = fill.time.gaps(d, time_interval)
   plot(d$time, d$value, type='l', xlab='', main='', ylab='', xlim=c(min(d$time), max(d$time)), ylim=c(0, max(d$value)), bty='L', lwd=5, col='darkgrey')
   par(mar=c(3,3,3,3))
-  if(return.values==T) d
+  invisible(d)
 }
 
-plot.wordcloud <- function(topic_term_matrix, topic_nr, wordsize_scale=0.75, relative_to_term_total=F){
+
+#' Function to plot a word cloud for a given topic
+#' 
+#' Can be passed to the plotfunction arguments in the \code{\link{createTopicBrowser}} function.
+#' 
+#' One of the standardized plotting function used in the Topicbrowser package to manage how topics and additional information are visualized.
+#' This specific function plots a wordcloud
+#' 
+#' One of lda_model, topic_term_matrix, or topicinfo needs to be specified
+#' 
+#' @param lda_model An lda model fitted using topicmodels::LDA
+#' @param topic_term_matrix a matrix of topics per term
+#' @param topicinfo The output of the \code{\link{clusterinfo}} function
+#' @param topic_nr the index number of a topic
+#' @param wordsize_scale a scale for the word sizes
+#' @param relative_to_term_total make word sizes relative
+#' @return nothing, only plots
+#' @export
+plot_wordcloud <- function(lda_model=NULL, topic_term_matrix=NULL, clusterinfo=NULL, topic_nr, wordsize_scale=0.75, relative_to_term_total=F){
+  if (is.null(topic_term_matrix)) {
+    if (is.null(clusterinfo)) {
+      if (is.null(lda_model)) stop("Either lda_model, topic_term_matrix, or topicinfo needs to be specified")
+      clusterinfo = clusterinfo(lda_model)
+    } 
+    topic_term_matrix = clusterinfo$topics_per_term
+  }
+  
   x = topic_term_matrix[topic_nr,]
   if(relative_to_term_total==T) {
     x = x / colSums(topic_term_matrix)
@@ -176,7 +202,7 @@ plot.wordcloud <- function(topic_term_matrix, topic_nr, wordsize_scale=0.75, rel
   par(mar=c(5,4,4,2) + 0.1) # reset to default
 }
 
-topics.plot.category <- function(document_sums, topic_nr, category_var, pct=T, value='relative'){
+plot_category <- function(document_sums, topic_nr, category_var, pct=T, value='relative'){
   p = par(mar=c(3,3,3,1))
   d = prepare.topics.plot.values(document_sums, break_var=as.character(category_var), topic_nr=topic_nr, pct=pct, value=value)
   colnames(d) = c('category','value')
--- R/topicbrowser.r
+++ R/topicbrowser.r
@@ -1,26 +1,3 @@
-#' Create a topic browser
-#' 
-#' This will create a set of linked html pages to browse the given topic model
-#' 
-#' @param info The output of the \code{\link{clusterinfo}} function
-#' @param plotfunction.overview A standardized function (see the `pf.` functions). Determines how the topics in the general topic overview are visualized 
-#' @param plotfunction.pertopic A standardized function (see the `pf.` functions). Determines what information is visualized in the pages per topic. Can be a list of multiple functions, in which case multiple plots are made. 
-#' @param ... Parameters to be passed to the plotfunctions.
-#' @param output an optional file name to write the browser html to
-#' @param nmaxdoc an integer, indicating the maximum number of top articles to be printed for each topic. 
-#' @return the html generated or (invisible) the filename
-#' @export
-createTopicBrowser <- function(info, 
-                               plotfunction.overview=pf.wordcloud, plotfunction.pertopic=c(pf.wordcloud, pf.topicdistribution), ...,  
-                               output=NULL, nmaxdoc=10, nmaxwords=100, browse=interactive(), topic_ids=1:nrow(info$topics_per_doc)) {
-  pf.parameters = list(...)
-  info$topic_ids = topic_ids
-  url = wrap_html(render_html(info, plotfunction.overview, plotfunction.pertopic, nmaxdoc, nmaxwords, pf.parameters),
-                  info = info, output=output)
-  if (browse) browseURL(url)
-  invisible(url)
-}
-
 #' Create a 'clusterinfo' object that can be used for the individual render methods
 #' 
 #' @param m: the fitted LDA model object from the topicmodels package
@@ -43,6 +20,28 @@ clusterinfo <- function(m, terms=NULL, documents=NULL, words=terms, meta=NULL) {
   
   # order meta for plots  
   list(tokens=tokens, wordassignments=wordassignments, topics_per_doc=topics_per_doc, topics_per_term=topics_per_term, meta=meta)
+}
+
+
+#' Create a topic browser
+#' 
+#' This will create a set of linked html pages to browse the given topic model
+#' 
+#' @param clusterinfo The output of the \code{\link{clusterinfo}} function
+#' @param plotfunction.overview A standardized function (see the `plot.` functions). Determines how the topics in the general topic overview are visualized 
+#' @param plotfunction.pertopic A standardized function (see the `plot.` functions). Determines what information is visualized in the pages per topic. Can be a list of multiple functions, in which case multiple plots are made. 
+#' @param output an optional file name to write the browser html to
+#' @param nmaxdoc an integer, indicating the maximum number of top articles to be printed for each topic. 
+#' @param ... Parameters to be passed to the plotfunctions.
+#' @return the html generated or (invisible) the filename
+#' @export
+createTopicBrowser <- function(clusterinfo, 
+                               plotfunction.overview=plot_wordcloud, plotfunction.pertopic=c(plot_wordcloud, plot_topicdistribution),  
+                               output=NULL, nmaxdoc=10, nmaxwords=100, browse=interactive(), topic_nrs=1:nrow(info$topics_per_doc), ...) {
+  url = wrap_html(render_html(clusterinfo, topic_nrs, plotfunction.overview, plotfunction.pertopic, nmaxdoc, nmaxwords, ...),
+                  clusterinfo=clusterinfo, output=output, topic_nrs=topic_nrs)
+  if (browse) browseURL(url)
+  invisible(url)
 }
 
 #' Extract a topic X document matrix from a topicmodels object
@@ -80,24 +79,24 @@ topicsPerTerm <- function(m) {
 #' @param output an optional filename to output to. If NULL, a tempfile will be used
 #' @return the output filename
 #' @export
-wrap_html <- function(wrapped, info, output=NULL) {
+wrap_html <- function(wrapped, clusterinfo, topic_nrs, output=NULL) {
   if (is.null(output)) {
     output = tempfile("topicbrowser_", fileext = ".html")
     message("Writing html to ", output)
   }
   sink(output)
   tryCatch({
-    cat(html_header(info$topic_ids))
+    cat(html_header(topic_nrs))
     force(wrapped)
     cat(html_footer())
   }, finally=sink())
   output
 }
 
-html_header <- function(topic_ids) {
+html_header <- function(topic_nrs) {
   TEMPLATE = system.file("template/template.html", package="topicbrowser", mustWork=T)
   html = readChar(TEMPLATE, file.info(TEMPLATE)$size)
-  css = paste(get_css(topic_ids), collapse="\n")
+  css = paste(get_css(topic_nrs), collapse="\n")
   parts = unlist(strsplit(html, "$CONTENT$", fixed = T))
   footer = parts[1]
   gsub("$CSS$", css, footer, fixed = T)
@@ -117,25 +116,25 @@ html_footer <- function() {
 #' 
 #' @param info the cluster_info object (list)
 #' @export
-render_html <- function(info, plotfunction.overview, plotfunction.pertopic, nmaxdoc, nmaxwords, pf.parameters) {
+render_html <- function(clusterinfo, topic_nrs, plotfunction.overview, plotfunction.pertopic, nmaxdoc, nmaxwords, ...) {
   # tabs
   cat('<ul class="nav nav-tabs" role="tablist" id="topictab">\n')
   cat('<li class="active"><a href="#home" role="tab" data-toggle="tab">Overview</a></li>\n')
-  for (topic_id in info$topic_ids) 
-    cat('<li><a href="#t', topic_id, '" role="tab" data-toggle="tab">Topic ', topic_id, '</a></li>\n', sep = "")
+  for (topic_nr in topic_nrs) 
+    cat('<li><a href="#t', topic_nr, '" role="tab" data-toggle="tab">Topic ', topic_nr, '</a></li>\n', sep = "")
   cat('</ul>\n')
   
   # content
   cat('<div class="tab-content">\n')
   cat('<div class="tab-pane fade in active" id="home">\n')
   message("Rendering overview")
-  render_overview(info, plotfunction.overview, pf.parameters)
+  render_overview(clusterinfo, topic_nrs, plotfunction.overview, ...)
   cat('</div>\n')
   
-  for (topic_id in info$topic_ids) {
-    message("Rendering topic ", topic_id)
-    cat('<div class="tab-pane fade in" id="t',topic_id,'">\n', sep="")
-    render_topic(topic_id, info, plotfunction.pertopic, nmaxdoc, nmaxwords, pf.parameters)
+  for (topic_nr in topic_nrs) {
+    message("Rendering topic ", topic_nr)
+    cat('<div class="tab-pane fade in" id="t',topic_nr,'">\n', sep="")
+    render_topic(clusterinfo, topic_nr,  plotfunction.pertopic, nmaxdoc, nmaxwords, ...)
     cat('</div>\n')
   }  
   cat('</div>\n')
@@ -156,17 +155,15 @@ tab_html <- function(id, name) {
 #' 
 #' @param info the cluster_info object (list)
 #' @export
-render_overview <- function(info, plotfunction, pf.parameters) {
-  for(topic_id in info$topic_ids){
-    cat('<a href="#" onclick="showTab(', topic_id, ');">', sep='')
+render_overview <- function(clusterinfo, topic_nrs, plotfunction, ...) {
+  for(topic_nr in topic_nrs){
+    cat('<a href="#" onclick="showTab(', topic_nr, ');">', sep='')
     
-    plotfunction_titled <- function(info, topic_nr){
-      plotfunction(info, topic_nr)
+    plotfunction_titled <- function(clusterinfo, topic_nr, ...){
+      plotfunction(clusterinfo=clusterinfo, topic_nr=topic_nr, ...)
       title(main=paste("Topic", topic_nr))
     }
-    parameters = pf.parameters[names(pf.parameters) %in% names(as.list(args(plotfunction_titled)))]
-    parameters = c(parameters, list(info=info, topic_nr = topic_id))
-    cat_plot(do.call(plotfunction_titled, parameters), width=300)
+    cat_plot(plotfunction_titled(clusterinfo, topic_nr, ...), width=300)
     cat("</a>")
   }
 }
@@ -194,19 +191,17 @@ plot_to_file <- function(plotfun, width=500, height=width) {
 #' @param topic_id: the topic id to render
 #' @param info the cluster_info object (list)
 #' @export
-render_topic <- function(topic_id, info, plotfunction, nmaxdoc, nmaxwords, pf.parameters) {
-  cat("<h1>Topic", topic_id, "</h1>")
+render_topic <- function(clusterinfo, topic_nr, plotfunction, nmaxdoc, nmaxwords, ...) {
+  cat("<h1>Topic", topic_nr, "</h1>")
   
-  for(pfunc in c(plotfunction)) {
-    parameters = pf.parameters[names(pf.parameters) %in% names(as.list(args(pfunc)))]
-    parameters = c(parameters, list(info=info, topic_nr = topic_id))
-    cat_plot(do.call(pfunc, parameters), width=500, height=500)
-  }
+  for(pfunc in c(plotfunction)) 
+    cat_plot(pfunc(clusterinfo=clusterinfo, topic_nr=topic_nr, ...), width=500, height=500)
+    
   cat("<h2>Top articles</h2>")
-  topicass = info$topics_per_doc[topic_id,]
+  topicass = clusterinfo$topics_per_doc[topic_nr,]
   docs = names(head(topicass[order(-topicass)], n=nmaxdoc))
   
-  for (doc in docs) render_article(doc, info, maxwords=nmaxwords)
+  for (doc in docs) render_article(clusterinfo, doc, maxwords=nmaxwords)
 }
 
 
@@ -219,23 +214,23 @@ render_topic <- function(topic_id, info, plotfunction, nmaxdoc, nmaxwords, pf.pa
 #' @param doc: the document id (dtm rowname) to render
 #' @param info the cluster_info object (list)
 #' @export
-render_article <- function(doc, info, maxwords=NULL) {
+render_article <- function(clusterinfo, doc, maxwords=NULL) {
   # header
   cat('<h3>', doc, '</h3>')
   
   # meta
   cat('<table>')
-  for (name in colnames(info$meta)) 
-    cat('<tr><th>', name, '   </th><td>', as.character(info$meta[info$meta$id == doc, name]), '</td></tr>')
+  for (name in colnames(clusterinfo$meta)) 
+    cat('<tr><th>', name, '   </th><td>', as.character(clusterinfo$meta[clusterinfo$meta$id == doc, name]), '</td></tr>')
   cat('</table>')
   
   
   # render words
-  if(!is.null(info$tokens)){
+  if(!is.null(clusterinfo$tokens)){
     cap <- function(x, n) if (is.null(n)) x else head(x,n)
-    tok = info$tokens[info$tokens$aid == doc, ]
+    tok = clusterinfo$tokens[clusterinfo$tokens$aid == doc, ]
     tok = tok[cap(order(tok$id), maxwords), ]
-    wa = info$wordassignments[info$wordassignments$aid == doc, c("term", "topic")]
+    wa = clusterinfo$wordassignments[clusterinfo$wordassignments$aid == doc, c("term", "topic")]
     topics = wa$topic[match(tok$term, wa$term)]
     cat(tagTokens(tok$word, topics))
   }
@@ -266,12 +261,12 @@ tagTokens <- function(tokens, topics){
 
 
 
-get_css <- function(topic_ids) {
+get_css <- function(topic_nrs) {
   CSS_TEMPLATE = system.file("template/style.css", package="topicbrowser", mustWork=T)
   css = readLines(CSS_TEMPLATE, warn=F)
-  colo = substr(rainbow(length(topic_ids), s=0.6,alpha=0.5), 1,7)
+  colo = substr(rainbow(length(topic_nrs), s=0.6,alpha=0.5), 1,7)
 
-  colorcss = paste(".t", topic_ids, " {background-color: ",colo, "}", sep="")
+  colorcss = paste(".t", topic_nrs, " {background-color: ",colo, "}", sep="")
   c(css, colorcss)
 }
 
